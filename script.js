3878c9bbe5ad1d8585eb7e78879fc3ea3eab733f
SurriyaLuavan <surriyaluavan@gmail.com>
2023-01-17 17:28:11 -0400

    Modified script.js and added game function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true});
+
 function getComputerChoice() {
     const val = ['rock', 'paper', 'scissors'];
     let randChoice = Math.floor(Math.random() * 3 );
@@ -16,12 +18,12 @@ function playRound (playerSelection, computerSelection) {
             break;
         case 'paper':
             if (computerSelection === 'scissors') return "You Lose! Scissors beats Paper";
-            else if (computerSelection === 'rock') return "You Win! Paper beats Rock";
+            else if (computerSelection === 'rock') return "You Won! Paper beats Rock";
             else return "It's a Draw! Both are Paper";
             break;
         case 'scissors':
             if (computerSelection === 'rock') return "You Lose! Rock beats Scissors";
-            else if (computerSelection === 'paper') return "You Win! Scissors beats Paper";
+            else if (computerSelection === 'paper') return "You Won! Scissors beats Paper";
             else return "It's a Draw! Both are Scissors";
             break;
         default: 
@@ -29,8 +31,24 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-//comp = getComputerChoice();
 
-//console.log(comp);
+function game(numOfGame) {
+    let userWin = 0;
+    let compWin = 0;
 
-//console.log(playRound ('Scissors', comp));
+    for (let i = 0; i < numOfGame; i++) {
+        let userOption = (prompt("Enter a valid option (rock, paper, scissors): "));
+        let compOption = getComputerChoice();
+        let result = playRound (userOption, compOption);
+        
+        console.log (result);
+        if (result.includes('Won')) userWin++;
+        if (result.includes('Lose')) compWin++;
+    }
+
+    console.log(`${userWin} ${compWin}`)
+
+    userWin > compWin ? console.log("The User Won!") : console.log("The User Lost!");
+}
+
+game(6);
