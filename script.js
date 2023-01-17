9198b785dd5fc91c026fe1379975e1198d83c816
SurriyaLuavan <surriyaluavan@gmail.com>
2023-01-17 16:41:14 -0400

    Added the playRound function to srcipt.js

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function getComputerChoice() {
-    const val = ['Rock', 'Paper', 'Scissors'];
+    const val = ['rock', 'paper', 'scissors'];
     let randChoice = Math.floor(Math.random() * 3 );
 
     return val[randChoice];
 }
+
+function playRound (playerSelection, computerSelection) {
+    
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection === 'scissors') return "You Won! Rock beats Scissors";
+            else if (computerSelection === 'paper') return "You Lose! Paper beats Rock";
+            else return "It's a Draw! Both are Rock";
+            break;
+        case 'paper':
+            if (computerSelection === 'scissors') return "You Lose! Scissors beats Paper";
+            else if (computerSelection === 'rock') return "You Win! Paper beats Rock";
+            else return "It's a Draw! Both are Paper";
+            break;
+        case 'scissors':
+            if (computerSelection === 'rock') return "You Lose! Rock beats Scissors";
+            else if (computerSelection === 'paper') return "You Win! Scissors beats Paper";
+            else return "It's a Draw! Both are Scissors";
+            break;
+        default: 
+            return "Please enter a valid option!";     
+    }
+}
+
+//comp = getComputerChoice();
+
+//console.log(comp);
+
+//console.log(playRound ('Scissors', comp));
